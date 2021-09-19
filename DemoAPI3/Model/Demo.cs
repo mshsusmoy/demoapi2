0542959a9c3c81f8b50bf945abc9fea286391fc9
mshsusmoy <mshsusmoy@gmail.com>
2021-09-19 23:30:06 +0600

    init

---
+++ DemoAPI3/Model/Demo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAPI3.Model
+{
+    public class Demo
+    {
+    }
+}
---
+++ DemoAPI3/Model/DemoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAPI3.Model
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options) 
+        :base(options){
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Demo> DemoItem { get; set; }
+    }
+}
